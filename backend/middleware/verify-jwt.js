de14a29be91082344957c85db2c3ec032dc48bb4
Amritesh <amriteshk778@gmail.com>
2023-10-31 18:12:55 +0530

    project finished signing off Amritesh

--- backend/middleware/verify-jwt.js
+++ backend/middleware/verify-jwt.js
@@ -15,7 +15,6 @@ const verifyJwt = asyncHandler(async (req, res, next) => {
           // console.log(token)
   
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        console.log(decoded)
   
         req.user = await User.findById(decoded.id).select("-password");
         // console.log(req.user);
